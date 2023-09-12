da927548c22b48370bb927d70015b25f03fdb248
ValerieBESCH-KARA <128912685+ValerieBESCH-KARA@users.noreply.github.com>
2023-09-12 21:43:59 +0200

    correction texte description

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -11,16 +11,16 @@ const AboutMePage = () => {
       <div className="infoMeBloc">
         <div className="infoMe">
           <p>
-            Je m'appelle Valérie BESCH-KARA, j'ai travaillé près de 10 ans à mon
-            compte en tant qu'ostéopathe sur Lyon avant de m'arrêter suite au
-            covid pour éléver mes 2 enfants. Je suis actuellement en pleine
-            reconversion professionnelle pour devenir développeur web.
+            Je m'appelle Valérie BESCH-KARA, je suis développeur web junior
+            depuis septembre 2023. Après avoir travaillé près de 10 ans en tant
+            qu'ostéopathe à mon propre compte à Lyon, j'ai voulu me réorienter
+            vers une discipline qui m'a toujours plu : l'informatique.
           </p>
           <p>
             J'ai toujours eu un attrait pour l'informatique qui s'est emplifié
             lorsque j'ai débuté le codage par moi-même. Par la suite j'ai suivi
-            une formation de 6 mois avec l'école "Le Reacteur" afin de poser de
-            vraies bases et d'acquérir de nouvelles compétences.
+            une formation de 6 mois avec l'école "Le Reacteur" afin de confirmer
+            mes bases et d'acquérir de nouvelles compétences.
           </p>
           <p>
             Passionnée par le développement web, j'aime éxpérimenter, découvrir
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,9 +1,23 @@
 import "../styles/ContactPage.css";
 
+import Github from "../assets/github.png";
+import Linkedin from "../assets/Linkedin.png";
+
 const ContactPage = () => {
   return (
     <div className="container contactPage">
-      <div>ContactPage component</div>
+      <h3>Contactez-moi</h3>
+
+      <h4>N'hésitez pas à me suivre sur :</h4>
+      <div className="contactLogoBloc">
+        <a href="https://github.com/ValerieBESCH-KARA">
+          <img src={Github} alt="logo" className="contactLogo" />
+        </a>
+
+        <a href="https://www.linkedin.com/in/valerie-besch-7b6755286/">
+          <img src={Linkedin} alt="logo" className="contactLogo" />
+        </a>
+      </div>
     </div>
   );
 };
